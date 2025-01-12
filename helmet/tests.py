3f7dee1cbc15996ac9aafa240862806529ea15cd
frankmathewsajan <145902617+frankmathewsajan@users.noreply.github.com>
2025-01-12 13:48:55 +0530

    sample unit tests

--- helmet/tests.py
+++ helmet/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class HelmetTests(TestCase):
+    def test_dashboard(self):
+        response = self.client.get('/dashboard')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'dashboard.html')
+        self.assertTemplateUsed(response, 'layout.html')
+
+    def test_login(self):
+        response = self.client.get('/login')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth/login.html')
+
+    def test_register(self):
+        response = self.client.get('/register')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auth/register.html')
